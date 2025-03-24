bbafdfc6094aea49f19469e4bdf58e85f5fc7e98
Oteayy <doroteasopic3@gmail.com>
2025-03-24 21:07:46 +0100

    summary

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from repositories import (init_database,
                           create_project,
                           create_task,
-                          get_project,
-                          get_projects)
+                          get_projects,
+                          get_project)
+
 
 def main():
     init_database()
-    project = ('Demo project', '2024-12-06', '2025-03-10')
+    project = ('Demo project 12', '2024-12-06', '2025-03-10')
     create_project(project)
-    
-    project_id = 1 # TODO Dohvatiti iz baze !!!
-    create_task(project_id)
-    
-    project = get_project(1)
+
+    project_id = 6 # TODO Dohvatiti iz baze!!!
+    task = ('Task 12', 1, 1,
+            '2024-12-06', '2025-03-10', project_id)
+    create_task(task)
+
+    project = get_project(5)
     print(project)
-    
+
     projects = get_projects()
     print(projects)
+
+
 
 if __name__ == '__main__':
     main()
--- repositories/db_repositories.py
+++ repositories/db_repositories.py
@@ -1,188 +1,121 @@
 import sqlite3
+from typing import Tuple
 
-from constants.constants import DATABASE
+from constants import (DATABASE,
+                       CREATE_TABLE_PROJECTS,
+                       CREATE_PROJECT,
+                       GET_PROJECTS,
+                       GET_PROJECT,
+                       CREATE_TABLE_TASKS,
+                       CREATE_TASK)
 
 
 def init_database():
-
-
-    DATABASE = 'database/projects.db'
-
-    create_tables = ['''
-        CREATE TABLE IF NOT EXISTS projects (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        begin_date DATE,
-        end_date DATE
-
-    );''',
-
-    '''
-        CREATE TABLE IF NOT EXISTS tasks (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        priority INTEGER,
-        project_id INTEGER NOT NULL,
-        status_id INTEGER NOT NULL,
-        begin_date DATE NOT NULL,
-        end_date DATE NOT NULL,
-        
-        FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE ON UPDATE CASCADE 
-    );
-    '''
+    create_tables_statements = [
+        CREATE_TABLE_PROJECTS,
+        CREATE_TABLE_TASKS
     ]
 
     try:
-        # 1.KORAK Kreiranje konekcijež
+        # 1. KORAK Kreiranje konekcije
         with sqlite3.connect(DATABASE) as conn:
-        
-        # 2.KORAK Kreiranje Cursor objekta za rad s bazom(predstavlja nasu bazu)
+            # 2. KORAK Kreiranje Cursor objekta za rad s bazom (predstavlja nasu bazu)
             cursor = conn.cursor()
 
-        # 3.KORAK Izvrsavanje SQL Query naredbi
-        for statement in create_tables:
-            cursor.execute(create_tables)
-            
+            # 3. KORAK Izvrsavanje SQL Query naredbi
+            for statement in create_tables_statements:
+                cursor.execute(statement)
+
             # Pokreni postupak snimanja promjena u bazi!!!
             conn.commit()
 
-        # 4.KORAK Zatvaranje konekcije na bazu - with automatski zatvara konekciju
-        # conn.close()
-        
+            # 4. KORAK Zatvarnje konekcije na bazu
+            # - with automatski zatvara konekciju
+            # conn.close()
     except sqlite3.OperationalError as e:
         print(f'Failed to open database: {e}')
-        
-    except Exception as ex:
-        print(f'Dogodila se greska {ex}.')
-        
 
-def create_project():
-    insert_project_statement = '''
-        INSERT INTO projects_name(name, begin_date, end_date)
-        VALUES(?, ?, ?)
-        '''
-        
-    try:
-        # 1.KORAK Kreiranje konekcijež
-        with sqlite3.connect(DATABASE) as conn:
-        
-        # 2.KORAK Kreiranje Cursor objekta za rad s bazom(predstavlja nasu bazu)
-            cursor = conn.cursor()
-
-        # 3.KORAK Izvrsavanje SQL Query naredbi
-        cursor.execute(insert_project_statement, ('Project name', '2025-01-25', '2025-02-08'))
-            
-            # Pokreni postupak snimanja promjena u bazi!!!
-        conn.commit()
-
-        # 4.KORAK Zatvaranje konekcije na bazu - with automatski zatvara konekciju
-        # conn.close()
-        
-    except sqlite3.OperationalError as e:
-        print(f'Failed to open database: {e}')
-        
     except Exception as ex:
         print(f'Dogodila se greska {ex}.')
 
+
+
+def create_project(project: Tuple):
+    projects_from_db = get_projects()
+    project_from_db = list(filter(lambda p: p[1] == project[0], projects_from_db))
+    if len(project_from_db) > 0:
+        # Update project
+        return
+    _commit_to_db(CREATE_PROJECT, project)
 
 def get_project(project_id: int):
-    get_project_statement = '''
-        SELECT * FROM projects
-        WHERE id = (?)
-        '''
-        
-        
+    return _get_from_db(GET_PROJECT, (project_id,))
+
+def get_projects():
+    return _get_from_db(GET_PROJECTS)
+
+def update_project(project_id: int, project: Tuple):
+    # TODO - dovrsiti funkciju tako da koristi neku od dolje kreiranih private funkcija
+    pass
+
+def delete_project(project_id: int):
+    # TODO - dovrsiti funkciju tako da koristi neku od dolje kreiranih private funkcija
+    pass
+
+
+def create_task(task):
+    # TODO Provjeriti postoji li ovaj projekt u bazi?
+    # Ako postoji azuriraj ga, a ako ne postoji kreiraj ga
+
+    _commit_to_db(CREATE_TASK, task)
+
+# TODO - ostale CRUD funkcija za Taskove
+# OPREZ - svaki task mora biti povezan uz project!!!
+
+
+
+def _commit_to_db(statement, data):
     try:
-        # 1.KORAK Kreiranje konekcije
+        # 1. KORAK Kreiranje konekcije
         with sqlite3.connect(DATABASE) as conn:
-        
-        # 2.KORAK Kreiranje Cursor objekta za rad s bazom(predstavlja nasu bazu)
+            # 2. KORAK Kreiranje Cursor objekta za rad s bazom (predstavlja nasu bazu)
             cursor = conn.cursor()
 
-        # 3.KORAK Izvrsavanje SQL Query naredbi
-        cursor.execute(get_project_statement, (project_id))
-        projects = cursor.fetchall()
-            
-            # Vrati podatke koji su dohvaceni iz baze
-        if len(projects) > 0:
-            return projects[0]
-        else:
-            return None
+            # 3. KORAK Izvrsavanje SQL Query naredbi
+            cursor.execute(statement, data)
 
-        # 4.KORAK Zatvaranje konekcije na bazu - with automatski zatvara konekciju
-        # conn.close()
-        
-    except sqlite3.OperationalError as e:
-        print(f'Failed to open database: {e}')
-        
-    except Exception as ex:
-        print(f'Dogodila se greska {ex}.')
-
-def create_projects(project: tuple):
-    get_project_statement = '''
-        SELECT * FROM projects
-        WHERE id = (?)
-        '''
-    projects_from_db = get_project
-    projects_from_db = list(filter(lambda p: p[1] == project[0], projects_from_db))
-    if len(projects_from_db) > 0:
-        return
-        
-    try:
-        # 1.KORAK Kreiranje konekcije
-        with sqlite3.connect(DATABASE) as conn:
-        
-        # 2.KORAK Kreiranje Cursor objekta za rad s bazom(predstavlja nasu bazu)
-            cursor = conn.cursor()
-
-        # 3.KORAK Izvrsavanje SQL Query naredbi
-        cursor.execute(get_project_statement)
-            
-            # Vrati podatke koji su dohvaceni iz baze
-        if len(project) > 0:
-            return project[0]
-        else:
-            return None
-
-        # 4.KORAK Zatvaranje konekcije na bazu - with automatski zatvara konekciju
-        # conn.close()
-        
-    except sqlite3.OperationalError as e:
-        print(f'Failed to open database: {e}')
-        
-    except Exception as ex:
-        print(f'Dogodila se greska {ex}.')
-
-
-def create_task(project_id: int):
-    insert_task_statement = '''
-        INSERT INTO tasks(name, priority, status_id, 
-                        begin_date, end_date,project_id,)
-        VALUES(?, ?, ?, ?, ?, ?)
-        '''
-        
-        # TODO Provjeriti postoji li ovaj projekt u bazi?
-        # Ako postoji azuriraj ga, a ako ne postoji kreiraj ga
-        
-    try:
-        # 1.KORAK Kreiranje konekcije
-        with sqlite3.connect(DATABASE) as conn:
-        
-        # 2.KORAK Kreiranje Cursor objekta za rad s bazom(predstavlja nasu bazu)
-            cursor = conn.cursor()
-
-        # 3.KORAK Izvrsavanje SQL Query naredbi
-        cursor.execute(insert_task_statement, 
-                       ('Task 1', 1, 1, '2025-01-31', '2025-02-10', project_id))
-            
             # Pokreni postupak snimanja promjena u bazi!!!
-        conn.commit()
+            conn.commit()
 
-        # 4.KORAK Zatvaranje konekcije na bazu - with automatski zatvara konekciju
-        # conn.close()
-        
     except sqlite3.OperationalError as e:
         print(f'Failed to open database: {e}')
-        
+
+    except Exception as ex:
+        print(f'Dogodila se greska {ex}.')
+
+def _get_from_db(statement, condition: Tuple = ()):
+    try:
+        # 1. KORAK Kreiranje konekcije
+        with sqlite3.connect(DATABASE) as conn:
+            # 2. KORAK Kreiranje Cursor objekta za rad s bazom (predstavlja nasu bazu)
+            cursor = conn.cursor()
+
+            # 3. KORAK Izvrsavanje SQL Query naredbi
+            if len(condition) > 0:
+                cursor.execute(statement, condition)
+            else:
+                cursor.execute(statement)
+
+            projects = cursor.fetchall()
+
+            # Vrati podatke koji su dohvaceni iz baze
+            if len(projects) > 0:
+                return projects
+            else:
+                return None
+
+    except sqlite3.OperationalError as e:
+        print(f'Failed to open database: {e}')
+
     except Exception as ex:
         print(f'Dogodila se greska {ex}.')
